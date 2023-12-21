9389fb84eac7738878a41c80a13b14e0e402570e
Omer Dolev <omerdolev90@gmail.com>
2023-12-21 18:34:58 +0200

    show books list

--- public/app.ts
+++ public/app.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+import { books } from "./books.js";
+
+const bookList = document.getElementById("book-list");
+
+if (bookList) {
+    bookList.innerHTML = books.map((book) => `<li>${book.title} - ${book.author}</li>`).join("\n");
+}
