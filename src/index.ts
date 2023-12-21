183c57d6fdbf42ef8b061e686841681768891da7
Omer Dolev <omerdolev90@gmail.com>
2023-12-21 19:48:32 +0200

    call books api from client

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,30 @@ import books from "./books.json";
 
 const app = express();
 
+// CRUD - Create Read Update Delete
 app.get("/api/books", (_, res) => {
+    // List books
     res.send(books);
+});
+
+app.get("/api/books/:id", (_, res) => {
+    // TODO: return book details
+});
+
+app.post("/api/books", (req, res) => {
+    // Create
+});
+
+app.put("/api/books/:bookId", (req, res) => {
+    // Replace
+});
+
+app.patch("/api/books/:id", (req, res) => {
+    // Update
+});
+
+app.delete("/api/books/:id", (req, res) => {
+    // Delete book
 });
 
 app.use(express.static("public"));
