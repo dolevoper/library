bb6ba3d39b2ace4a1061ff1b2967db26a1d9d6e9
Omer Dolev <53278705+dolevoper@users.noreply.github.com>
2023-12-24 22:18:45 +0000

    copies stub

--- public/book-details.ts
+++ public/book-details.ts
@@ -1,8 +1,11 @@
-import { getBookDetails } from "./books.js";
+import { getBookDetails, getCopies } from "./books.js";
 
 async function app() {
     const bookId = window.location.hash.slice(1);
-    const bookDetails = await getBookDetails(bookId);
+    const [bookDetails, copies] = await Promise.all([
+        getBookDetails(bookId),
+        getCopies(bookId)
+    ]);
 
     const coverImage = document.getElementById("book-cover");
     coverImage?.setAttribute("src", `https://github.com/benoitvallon/100-best-books/blob/master/static/${bookDetails.imageLink}?raw=true`);
@@ -12,6 +15,14 @@ async function app() {
     renderBookField("language");
     renderBookField("year");
     renderBookField("pages");
+
+    const bookCopies = document.getElementById("book-copies");
+
+    if (!bookCopies) {
+        throw new Error();
+    }
+
+    bookCopies.innerText = copies.length.toString();
 
     function renderBookField(field: keyof typeof bookDetails) {
         const authorSpan = document.getElementById(`book-${field}`);
--- public/books.ts
+++ public/books.ts
@@ -10,7 +10,12 @@ type Book = {
   year: number
 };
 
-export async function getBooks(): Promise<Book[]> {
+type Copy = {
+  id: string,
+  bookId: string
+};
+
+export async function getBooks(): Promise<Pick<Book, "id" | "title" | "author">[]> {
   const res = await fetch("/api/books");
 
   return res.json();
@@ -20,4 +25,11 @@ export async function getBookDetails(bookId: string): Promise<Book> {
   const res = await fetch(`/api/books/${bookId}`);
 
   return res.json();
+}
+
+export async function getCopies(bookId: string): Promise<Copy[]> {
+  return [{
+      id: crypto.randomUUID(),
+      bookId
+  }];
 }
